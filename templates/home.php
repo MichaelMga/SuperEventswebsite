db56a336933d70489c4d87c4fba243622475b79c
michaelMga <middleweightsoul@gmail.com>
2022-06-30 16:16:14 +0200

    AFB 01 add createEvent form + js script

--- templates/home.php
+++ templates/home.php
@@ -2,17 +2,25 @@
   <h1>Bienvenue chez AFB</h1>
 
 </br>
+   <form id="searchEventForm" action="<?php echo rootUrl ?>">
+     <input id="eventInput" type="text" placeholder="search your event">
+     <button onclick="executeLoadingScript(event)">Chercher</button>
+ </form>
 
-
-<form id="myForm" action="<?php echo rootUrl ?>?event=" method="post">
-   <input id="eventInput" type="text" placeholder="search your event">
+ <form id="createEventForm" action="<?php echo rootUrl ?>">
+   <input id="createEventInput" type="text" placeholder="search your event">
    <button onclick="executeLoadingScript(event)">Chercher</button>
 </form>
+
 
 <script>
 
   const executeLoadingScript = (event) => {
-    document.getElementById("myForm").action += document.getElementById("eventInput").value;
+    document.getElementById("searchEventForm").action += document.getElementById("eventInput").value;
+  }
+
+  const createEvent = (event) => {
+    document.getElementById("createEventForm").action += document.getElementById("createEventInput").value;
   }
 
 </script>
