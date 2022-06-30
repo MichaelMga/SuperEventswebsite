b44b0e0db219ef67e34fe1675098299d41a5006d
michaelMga <middleweightsoul@gmail.com>
2022-06-30 16:21:56 +0200

    AFB 01 add create event route

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,5 +1,6 @@
 <?php
 
  $routes = [
-  rootUrl => ["name" => "home", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["home", []]  ]
+  rootUrl => ["name" => "home", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["home", []]  ],
+  rootUrl . 'create' => ["name" => "create", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["admin/projects/new", []]  ]
 ];
