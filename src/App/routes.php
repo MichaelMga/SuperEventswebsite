661a29c6a2be363652dc83992cd59192ff3c30c5
michaelMga <middleweightsoul@gmail.com>
2022-07-03 20:14:05 +0200

    AFB04 update createEvent route

--- src/App/routes.php
+++ src/App/routes.php
@@ -6,6 +6,8 @@
   rootUrl . 'testEvent' => ["name" => "create", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["events/test", []]  ],
   rootUrl . 'payment' => ["name" => "login", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["payment/stripeForm", []]  ],
 
-  rootUrl . 'admin' => ["name" => "admin", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["admin/admin", []]  ]
+  rootUrl . 'admin' => ["name" => "admin", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["admin/admin", []]  ],
+
+  rootUrl . 'events/create' => ["name" => "new event", "controller" => "App\Controllers\AdminController" , "method" =>  "createNewEvent" , "parameters" => ["admin/admin", []]  ],
 
   ];
