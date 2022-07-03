d571bfb312a4538526bc83a14b3d04c930d2e612
michaelMga <middleweightsoul@gmail.com>
2022-07-03 19:50:18 +0200

    AFB04 build new admin route

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,8 +1,11 @@
 <?php
 
+
  $routes = [
   rootUrl => ["name" => "home", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["home", []]  ] ,
   rootUrl . 'testEvent' => ["name" => "create", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["events/test", []]  ],
-  rootUrl . 'payment' => ["name" => "login", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["payment/stripeForm", []]  ]
+  rootUrl . 'payment' => ["name" => "login", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["payment/stripeForm", []]  ],
+
+  rootUrl . 'admin' => ["name" => "admin", "controller" => "App\Controllers\FrontController" , "method" =>  "displayPage" , "parameters" => ["admin/admin", []]  ]
 
   ];
