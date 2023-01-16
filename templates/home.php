3faed33c105ab194878547aa9ee39cde69322fca
michaelMga <middleweightsoul@gmail.com>
2023-01-16 20:50:53 +0100

    Move style declarations from template to css file

--- templates/home.php
+++ templates/home.php
@@ -1,8 +1,8 @@
 
-<div class="homeContainer" style="background-image: url('https://images.pexels.com/photos/593324/pexels-photo-593324.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2') ; background-size:cover; position: absolute; top: 0; left:0; width:100%; min-height: 100%; height:100%; display:flex; flex-direction: column; justify-content: center; align-items: center">
+<div class="homeContainer">
 
- <div class="" style="height: 60%; width: 80%">
-   <div style="height: 20%;">
+ <div class="homeCenterDiv">
+   <div class="homeCenterDivTextContainer">
      <h1 style="color: white">Bienvenue chez vous </h1>
    </div>
 
