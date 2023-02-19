86b26557711d9b45005282e965473aa78bddd180
michaelMga <middleweightsoul@gmail.com>
2023-02-19 15:08:14 +0100

    AFB import typeform in mail template

--- templates/mail/form.php
+++ templates/mail/form.php
@@ -2,6 +2,16 @@
   Here is your form
 </h1>
 
+<!-- Add the following code where you want to display the form -->
+	<div class="typeform-widget" data-url="YOUR_TYPEFORM_URL" data-transparency="50" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 500px;"></div>
+	<!-- Add the following script to handle the form submission and redirect to another page -->
+	<script>
+		var form = document.querySelector('.typeform-widget');
+		form.addEventListener('submit', function() {
+			var email = encodeURIComponent(document.querySelector('input[name="email"]').value);
+			window.location.href = "YOUR_REDIRECT_PAGE_URL?email=" + email;
+		});
+	</script>
 
   <input id="userMailInput" placeholder="Adresse mail"/>
   <input id="userMailConfirmationInput" placeholder="Confirmation Adresse mail"/>
